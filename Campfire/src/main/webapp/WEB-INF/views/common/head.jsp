465e7d69e5fd6b887e78df9a5d17012e2064d57c
Administrator <Administrator@DESKTOP-2F0BILV>
2023-08-01 10:59:39 +0900

    프로젝트 시작

---
+++ Campfire/src/main/webapp/WEB-INF/views/common/head.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<head>
+	<title>campfire</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="../../resources/css/main.css" />
+	<link rel="stylesheet" href="../../resources/css/inquiry.css" />
+</head>
---
+++ Campfire/src/main/webapp/resources/js/changeListStyle.js
@@ -0,0 +1,20 @@
+const menuLogo1 = document.getElementById('menu-logo1');
+const menuLogo2 = document.getElementById('menu-logo2');
+
+const listStyle1 = document.getElementById('ls1');
+const listStyle2 = document.getElementById('ls2');
+
+menuLogo1.addEventListener('click', function() {
+    menuLogo1.src = '../images/icon2-select.png';
+    menuLogo2.src = '../images/icon1.png';
+    listStyle1.style.display = 'block'
+    listStyle2.style.display = 'none'
+
+});
+
+menuLogo2.addEventListener('click', function() {
+    menuLogo1.src = '../images/icon2.png';
+    menuLogo2.src = '../images/icon1-select.png';
+    listStyle1.style.display = 'none'
+    listStyle2.style.display = 'block'
+});
